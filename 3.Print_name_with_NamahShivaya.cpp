776e4392ea62f2f59207dcb4ae3f207ed47c8301
KarthikS2004 <karthikshibi123@gmail.com>
2025-08-31 22:30:05 +0530

    name

---
+++ 3.Print_name_with_NamahShivaya.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string Name;
+	cout << "Enter the Name:";
+	cin >> Name;
+	cout  << "Namah Shivaya," << Name << "!" << endl;
+
+return 0;
+}
+	
