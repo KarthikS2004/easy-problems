c53044f9af6ff3a95d0c7e0a400fcd477615c208
KarthikS2004 <karthikshibi123@gmail.com>
2025-08-31 22:47:15 +0530

    Area

---
+++ 4.Area_of_a_rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int length,width;
+	cout<<"Enter the Length:";
+	cin>>length;
+	cout<<"Enter the Width:";
+	cin>>width;
+	int area=length*width;
+	cout<<"The area of the Rectangle:"<<area;
+return 0;
+}
