423865aa31b6b5c4032bf704060e25806f87a4ef
KarthikS2004 <karthikshibi123@gmail.com>
2025-08-31 21:41:22 +0530

    Initial commit of my project

---
+++ 2.Sum_of_two_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    
+    cout << "Enter the number1:";
+    cin >> a;
+    
+    cout << "Enter the number2:";
+    cin >> b;
+    
+    int Sum=a+b;
+    
+    cout << "Sum of two number is:" << Sum << endl;
+    return Sum;
+}
+
