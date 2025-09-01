fd05d35549a25647fe6402456c374edbc5003fda
KarthikS2004 <karthikshibi123@gmail.com>
2025-09-02 01:55:15 +0530

    Area

---
+++ 16.Area_of_a_Circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int radius=20;
+    float area = 3.14*radius*radius;
+	cout << "Area of the Circle is:" << area << endl;
+
+return 0;
+}
