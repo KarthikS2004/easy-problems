81982fda2ec3d50611517465f53bde68196dfd01
KarthikS2004 <karthikshibi123@gmail.com>
2025-09-02 01:59:30 +0530

    Circumference

---
+++ 17.Circumference_of_a_Circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int radius=20;
+    float circumference = 2*3.14*radius;
+	cout << "Circumference of the Circle is:" << circumference << endl;
+
+return 0;
+}
