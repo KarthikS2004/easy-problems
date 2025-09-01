d8c15b2729a603763701b4e729db226733e605fd
KarthikS2004 <karthikshibi123@gmail.com>
2025-09-02 01:03:58 +0530

    Larger

---
+++ 12.Largest_of_two_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1,number2;
+cout << "Enter a number1 and number2:"<< endl;
+	cin >> number1 >> number2;
+
+
+    if(number1 > number2){
+	cout << number1 << " is Larger than " << number2;
+}
+    else{
+	cout << number2 << " is Larger than : " << number1;	
+  
+return 0;
+}
+}
